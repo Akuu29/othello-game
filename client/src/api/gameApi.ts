09d08935352a753a768d4fa8886424ee3377e9fc
Akuu29 <owner.akiu0529@gmail.com>
2022-07-11 20:02:48 +0900

    Update client.

--- client/src/api/gameApi.ts
+++ client/src/api/gameApi.ts
@@ -1,23 +1,104 @@
-import axios from "axios";
+import axios, { AxiosError, AxiosResponse } from "axios";
 
 export type BoardMap = Array<Array<number>>;
 
+interface ResponseBasic {
+    status: "success" | "error";
+}
+
+interface ResponseOnSuccess extends ResponseBasic{
+    status: "success";
+    nextMove?: "calculateWinner" | "continue" | "path";
+    board?: BoardMap;
+    winner?: string;
+}
+
+interface ResponseOnError extends ResponseBasic {
+    status: "error";
+}
+
+type Response = AxiosResponse<ResponseOnSuccess | ResponseOnError> | undefined;
+
 export class GameApi {
-    static async startGame(): Promise<BoardMap> {
-        let response = await axios.get(process.env.REACT_APP_BACKEND_URL!);
-        return await response.data.board;
+    static async getInitialBoard(): Promise<Response> {
+        const response = await axios
+            .get(process.env.REACT_APP_BACKEND_URL!)
+            .catch((error: AxiosError | Error) => {
+                return handleAxiosError(error);
+            });
+
+        return response;
     }
-    static async setStoneInBoard(stonePosition: Array<number>, squares: BoardMap, player: number): Promise<BoardMap | null> {
-        let body = {
+    // static async getPlayerIsReversible(squares: BoardMap, player: number): Promise<Response> {
+    //     const config = {
+    //         params: {
+    //             stonePosition: [0, 0],
+    //             board: squares,
+    //             player
+    //         }
+    //     };
+
+    //     const response = await axios
+    //         .get(`${process.env.REACT_APP_BACKEND_URL!}/player-is-reversible`, config)
+    //         .catch((error: AxiosError | Error) => {
+    //             return handleAxiosError(error);
+    //         });
+
+    //     return response;
+    // }
+    static async updateBoard(
+        stonePosition: Array<number>,
+        squares: BoardMap,
+        player: number): Promise<Response> {
+        const body = {
             stone_position: stonePosition,
             board: squares,
             player
         };
-        let response = await axios
-            .post(process.env.REACT_APP_BACKEND_URL!, body)
-            .catch(() => {
-                return null;
+        const response: Response = await axios
+            .put(process.env.REACT_APP_BACKEND_URL!, body)
+            .catch((error: AxiosError | Error) => {
+                return handleAxiosError(error);
             });
-        return response ? await response.data.board : response;
+
+        return response;
     }
+    // static async getTips() {
+
+    // }
+    static async getWinner(squares: BoardMap): Promise<Response> {
+        const body = {
+            stone_position: [0, 0],
+            board: squares,
+            player: 0
+        };
+        // const config = {
+        //     params: {
+        //         squares
+        //     }
+        // };
+        const response = await axios
+            // .get(`${process.env.REACT_APP_BACKEND_URL!}/winner`, config)
+            .post(`${process.env.REACT_APP_BACKEND_URL!}/winner`, body)
+            .catch((error: AxiosError | Error) => {
+                return handleAxiosError(error);
+            })
+
+        return response;
+    }
+}
+
+const handleAxiosError = (error: AxiosError<ResponseOnError> | Error): AxiosResponse<ResponseOnError> | undefined => {
+    if(axios.isAxiosError(error)) {
+        // AxiosError
+        if(error.response && typeof error.response.data != "object") {
+            const data: ResponseOnError = {
+                status: "error"
+            };
+            error.response.data = data;
+        }
+        return error.response;
+    }
+    // Error
+    return undefined;
 }
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,7 +5,7 @@ import './index.css';
 
 const Square: FC<{
     squareValue: number;
-    handleClick: MouseEventHandler
+    handleClick: MouseEventHandler;
 }> = ({squareValue, handleClick}) => {
     const stone = squareValue === 1 ? '●' :
         squareValue === 2 ? '○' : null;
@@ -16,28 +16,13 @@ const Square: FC<{
     );
 };
 
-const Board: FC = () => {
-    const [squares, setSquares] = useState<BoardMap>();
-    const [blackIsNext, setblackIsNext] = useState<boolean>(true);
-
-    // 初期版面の設定
-    useEffect(() => {
-        const setInitialBoard = async () => {
-            let initializeBoard = await GameApi.startGame();
-            setSquares(initializeBoard);
-        };
-
-        setInitialBoard();
-    }, [])
-
-    const handleClick = async (h: number, w: number) => {
-        const player = blackIsNext ? 1 : 2;
-        let boardSettedStone = await GameApi.setStoneInBoard([h, w], squares!, player);
-        if(boardSettedStone) {
-            setSquares(boardSettedStone);
-            setblackIsNext(!blackIsNext);
-        }
-    };
+const Board: FC<{
+    squares: BoardMap | undefined;
+    handleClick: any;
+}> = ({
+    squares,
+    handleClick
+}) => {
 
     const renderSquare = (line: Array<number>, h: number) => {
         return line.map((_, w) => (
@@ -47,35 +32,188 @@ const Board: FC = () => {
         ))
     };
 
-    const renderInitializeBoard = () => {
-        if(squares) {
-            return squares.map((line, h) => (
-                <div className="board-row">
-                    {renderSquare(line, h)}
-                </div>
-            ))
-        }
+    const renderBoard = () => {
+        return (
+            <div>
+                {squares && squares!.map((line, h) => (
+                    <div className="board-row">
+                        {renderSquare(line, h)}
+                    </div>
+                ))}
+            </div>
+        )
     };
-
-    const status = `Next player: ${blackIsNext ? '●' : '○'}`;
 
     return (
         <div>
-            <div className="status">{status}</div>
-            {renderInitializeBoard()}
+            {renderBoard()}
         </div>
     );
 };
 
 const Game: FC = () => {
+    const [squares, setSquares] = useState<BoardMap>();
+    const [squaresHistories, setSquaresHistories] = useState<Array<BoardMap>>([]);
+    const [blackIsNext, setBlackIsNext] = useState<boolean>(true);
+    const [blackIsNextHistories, setBlackIsNextHistories] = useState<Array<boolean>>([true]);
+    const [stepNumber, setStepNumber] = useState<number>(0);
+    // const [pathCount, setPathCount] = useState<number>(0);
+    const [winner, setWinner] = useState<string>();
+
+    // 初期版面の設定
+    useEffect(() => {
+        setInitialBoard();
+    }, [])
+
+        // // プレイヤーが切り替わった時に、そのプレイヤーが石の配置が可能か確認する
+    // // 配置不可能な場合、次のプレイヤーにパスする
+    // // 次のプレイヤーも配置不可能の場合は、ゲーム終了とする
+    // useEffect(() => {
+    //     const calculateWinner = async () => {
+    //         const resultGetWinner = await GameApi.getWinner(squares!);
+    //         if(resultGetWinner) {
+    //             const data = resultGetWinner.data;
+    //             if(data.status === "success") {
+    //                 const winner = data.winner;
+    //                 // 勝者のポップアップ
+    //                 // TODO
+    //             }else {
+    //                 // AxiosError
+    //                 alert(`ERROR: ${resultGetWinner.status}`);
+    //             }
+    //         }else {
+    //             // Error
+    //             alert("ERROR");
+    //         }
+    //     };
+    //     const calculatePlayerIsReversible = async () => {
+    //         const player = blackIsNext ? 1 : 2;
+    //         const resultGetPlayerIsReversible = await GameApi.getPlayerIsReversible(squares!, player);
+    
+    //         if(resultGetPlayerIsReversible) {
+    //             const data = resultGetPlayerIsReversible.data;
+    //             if(data.status === "success") {
+    //                 if(data.nextMove === "path") {
+    //                     setBlackIsNext(!blackIsNext);
+    //                     setPathCount((prevPathConut) => prevPathConut++);
+    //                     if(pathCount >= 2) {
+    //                         calculateWinner();
+    //                     }
+    //                 }
+    //             }else {
+    //                 // AxiosError
+    //                 alert(`ERROR: ${resultGetPlayerIsReversible.status}`);
+    //             }
+    //         }else {
+    //             // Error
+    //             alert("ERROR");
+    //         }
+    //     };
+
+    //     calculatePlayerIsReversible();
+    // }, [blackIsNext])
+
+    // ボードの初期化
+    const setInitialBoard = async () => {
+        const resultGetInitialBaord = await GameApi.getInitialBoard();
+        if(resultGetInitialBaord) {
+            const data = resultGetInitialBaord.data;
+            if(data.status === "success") {
+                const board = data.board;
+                setSquares(board);
+                setSquaresHistories((prevHistory) => {
+                    return prevHistory.concat([board!]);
+                });
+            }else {
+                // AxiosError
+                alert(`ERROR: ${resultGetInitialBaord.status}`);
+            }
+        }else {
+            // Error
+            alert("ERROR");
+        }
+    };
+
+    const handleClick = async (h: number, w: number) => {
+        const player = blackIsNext ? 1 : 2;
+        const resultUpdateBoard = await GameApi.updateBoard([h, w], squares!, player);
+        if(resultUpdateBoard) {
+            const data = resultUpdateBoard.data;
+            if(data.status === "success") {
+                if(data.nextMove === "calculateWinner") {
+                    calculateWinner(data.board!);
+                }
+                setBlackIsNext(!blackIsNext);
+                setBlackIsNextHistories((prevHistory) => {
+                    return prevHistory.concat(!blackIsNext);
+                })
+                setSquares(data.board);
+                setSquaresHistories((prevHistory) => {
+                    return prevHistory.concat([data.board!]);
+                });
+                setStepNumber((prevStepNumber) => prevStepNumber + 1);
+            }else {
+                // AxiosError
+                // alert(`ERRROR: ${resultUpdateBoard.status}`);
+            }
+        }else {
+            // Error
+            alert("ERROR");
+        }
+    };
+
+    const calculateWinner = async (board: BoardMap) => {
+        const resultGetWinner = await GameApi.getWinner(board);
+        if(resultGetWinner) {
+            const data = resultGetWinner.data;
+            if(data.status === "success") {
+                const winner = data.winner;
+                setWinner(winner);
+            }else {
+                // AxiosError
+                alert(`ERROR: ${resultGetWinner.status}`);
+            }
+        }else {
+            // Error
+            alert("ERROR");
+        }
+    };
+
+    const movesBtn = () => {
+        const move = (movename: string) => {
+            const action = movename === "prev" ? -1 : 1;
+            const squaresHistory = squaresHistories[stepNumber + action];
+            const blackIsNextHistory = blackIsNextHistories[stepNumber + action];
+            setSquares(squaresHistory);
+            setBlackIsNext(blackIsNextHistory);
+            setStepNumber((prevStepNumber) => prevStepNumber + action);
+        }
+        return (
+            <>
+                {(stepNumber > 0) && 
+                    <button onClick={() => move("prev")}>prev</button>
+                }
+                {(stepNumber < squaresHistories.length - 1) &&
+                    <button onClick={() => move("next")}>next</button>
+                }
+            </>
+        );
+    };
+
+    let status = `Next player: ${blackIsNext ? '●' : '○'}`;
+    if(winner) status = winner;
+
     return (
         <div className="game">
             <div className="game-board">
-                <Board />
+                <Board 
+                    squares={squares}
+                    handleClick={handleClick}
+                />
             </div>
             <div className="game-info">
-                <div>{/* status */}</div>
-                <ol>{/* TODO */}</ol>
+                <div>{status}</div>
+                <ol>{movesBtn()}</ol>
             </div>
         </div>
     );
